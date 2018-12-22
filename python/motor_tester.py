25ffc8b694ea060ad8784397adba9c3e63caa93a
prat-degwekar <prat.degwekar@gmail.com>
2018-12-22 19:17:56 +0530

    cleaned code, added usability wrapper functions

--- python/motor_tester.py
+++ python/motor_tester.py
@@ -19,6 +19,17 @@ def stop( pin, sleep_factor ):
     pi.set_servo_pulsewidth( pin, 1500 )
     sleep( sleep_factor )
 	
+def move_motor( pin, width, sleep_factor ):		#moves motor on pin "pin" by pulsewidth "width" ending with a sleep call
+    pi.set_servo_pulsewidth( pin, width )
+    sleep( sleep_factor )
+	
+def pulser ( direction, percentage ):		#given percentage of speed required, returns pulse width
+    cutdown = 100.00 - percentage
+    if direction == "forward" :
+        return ( 1750 - ( ( cutdown / 100 ) * 250 ) )
+    else:
+        return ( 1250 + ( ( cutdown / 100 ) * 250 ) )
+		
 
 #Access RPI GPIO
 pi = pigpio.pi()
@@ -27,61 +38,32 @@ gpio.setmode(gpio.BOARD);
 
 esc_gpio = 18
 sleep_ms = 5
+limit_up = 1750
+limit_down = 1250
 gpio.setup(esc_gpio,gpio.OUT);
 
-'''
-#pi.set_servo_pulsewidth(esc_gpio,0 ) # Maximum throttle.
-#sleep(2)
-#pi.set_servo_pulsewidth(esc_gpio, 1000) # Minimum throttle.
-#sleep(1)
-#pi.set_servo_pulsewidth(esc_gpio,1250);
-#sleep(1)
-'''
-
-#pi.set_servo_pulsewidth(esc_gpio,1500);
-#sleep(5);
-
-# Decide a required step factor of speed or whether to send pulse width itself and change accordingly
 
 stop ( esc_gpio, sleep_ms )
 
 print('forward')
-#pi.set_servo_pulsewidth(esc_gpio,1750)
-#sleep(5);
-forward ( esc_gpio, 1750, sleep_ms )
+
+forward ( esc_gpio, limit_up, sleep_ms )	#can replace with move_motor
 
 print('stop')
-#pi.set_servo_pulsewidth(esc_gpio, 1600);
-#sleep(5)
 
-forward ( esc_gpio, 1600, sleep_ms )
-
-#pi.set_servo_pulsewidth(esc_gpio,1500);
-#sleep(5);
+forward ( esc_gpio, pulser( "forward", 50 ), sleep_ms )
 
 stop ( esc_gpio, sleep_ms )
 
 print('back')
 
-#pi.set_servo_pulsewidth(esc_gpio,1400);
-#sleep(5)
+backward ( esc_gpio, pulser( "backward", 50 ), sleep_ms )
 
-backward ( esc_gpio, 1400, sleep_ms )
-
-#pi.set_servo_pulsewidth(esc_gpio,1250);
-#sleep(5);
-
-backward ( esc_gpio, 1250, sleep_ms )
+backward ( esc_gpio, limit_down, sleep_ms )
 
 print('stop')
 
-#pi.set_servo_pulsewidth(esc_gpio,1400);
-#sleep(5)
-
 backward ( esc_gpio, 1400, sleep_ms )
-
-#pi.set_servo_pulsewidth(esc_gpio,1500);
-#sleep(5);
 
 stop ( esc_gpio, sleep_ms )
 
