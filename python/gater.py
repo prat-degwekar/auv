bc79381a1641b853deff4c0cff235db86cbfddeb
reisub0 <reisub0@gmail.com>
2019-02-26 17:49:53 +0530

    Adds basic edge detection

--- python/gater.py
+++ python/gater.py
@@ -1,8 +1,25 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-img = cv2.imread( '''path of image''', 0)
+img = cv2.imread( 'gate_try1.jpeg', 0)
 
-cv2.imshow('image', img)
-cv2.waitkey(0)
-cv2.destroyAllWindows()
+#edges = cv2.Canny(img, 250, 250)
+
+lower_thresh = 100
+upper_thresh = 200
+apSize = 3 #make 5 or 7 for increasing sensitivity
+
+edges = cv2.Canny(img, lower_thresh, upper_thresh, apertureSize = apSize)
+
+plt.subplot(121), plt.imshow(img, cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(122), plt.imshow(edges, cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+plt.show()
+
+#cv2.imshow('image', img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
